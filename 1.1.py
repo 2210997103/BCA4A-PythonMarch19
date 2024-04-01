72e90b479086fc53726a45fe904bac0190c660f6
2210997103 <163818232+2210997103@users.noreply.github.com>
2024-04-01 18:45:14 +0530

    Add files via upload

---
+++ 1.1.py
@@ -0,0 +1,9 @@
+print ("name ishika rollno 2210997103")
+
+
+length=int(input("enter the length of the rectangle"))
+breadth=int(input("enter the breadth of the rectangle"))
+area=length*breadth
+perimeter=2*(length+breadth)
+print("the area of  the rectangle is:",area)
+print("the perimeter of the rectangle:",perimeter)
